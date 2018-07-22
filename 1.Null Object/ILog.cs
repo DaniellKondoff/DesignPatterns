e5ea75e34601b432b20f111de6e9751149b96903
Daniel Kondov <danielkondov91@gmail.com>
2018-07-22 21:00:17 +0300

    Null Onject

---
+++ 1.Null Object/ILog.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NullObject
+{
+    public interface ILog
+    {
+        void Info(string msg);
+        void Warn(string msg);
+    }
+}
---
+++ 1.Null Object/StartUp.cs
@@ -0,0 +1,25 @@
+﻿using Autofac;
+using System;
+
+namespace NullObject
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var log = new ConsoleLog();
+            var bankAccount = new BankAccount(log);
+            bankAccount.Deposit(100);
+
+            var cb = new ContainerBuilder();
+            cb.RegisterType<BankAccount>();
+            cb.RegisterType<NullLog>().As<ILog>();
+
+            using (var c = cb.Build())
+            {
+                var ba = c.Resolve<BankAccount>();
+                ba.Deposit(100);
+            }
+        }
+    }
+}
