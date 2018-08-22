2e6a8e5ec6cf8a525c2bb0303828844395907a55
Daniel Kondov <danielkondov91@gmail.com>
2018-08-22 21:20:36 +0300

    Prototype

---
+++ 4.Prototype/Prototype/Address.cs
@@ -0,0 +1,24 @@
+﻿namespace Prototype
+{
+    public class Address
+    {
+        public readonly string StreetName;
+        public int HouseNumber;
+
+        public Address(string streetName, int houseNumber)
+        {
+            StreetName = streetName;
+            HouseNumber = houseNumber;
+        }
+
+        public Address DeepCopy()
+        {
+            return new Address(StreetName, HouseNumber);
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(StreetName)}: {StreetName}, {nameof(HouseNumber)}: {HouseNumber}";
+        }
+    }
+}
---
+++ 4.Prototype/Prototype/Person.cs
@@ -0,0 +1,25 @@
+﻿namespace Prototype
+{
+    public class Person
+    {
+        public readonly string[] Names;
+        public readonly Address Address;
+
+        public Person(string[] names, Address address)
+        {
+            Names = names;
+            Address = address;
+        }
+
+        public Person DeepCopy()
+        {
+            return new Person(this.Names, Address.DeepCopy());
+        }
+
+        public override string ToString()
+        {
+            return $"{nameof(Names)}: {string.Join(",", Names)}, {nameof(Address)}: {Address}";
+        }
+
+    }
+}
