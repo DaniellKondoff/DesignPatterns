893920a6e5fa0a5cc57fac97684529b3da460b3e
Daniel Kondov <danielkondov91@gmail.com>
2018-08-28 22:13:26 +0300

    Bridge

---
+++ Bridge/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Bridge
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            IRenderer render = new VectorRenderer();
+            IRenderer render2 = new RasterRenderer();
+            var circle = new Circle(render2, 5);
+            circle.Draw();
+            circle.Resize(5);
+            circle.Draw();
+        }
+    }
+}
