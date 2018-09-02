447dc4c9a79bb11c9f6eb395c68f7f39c005d123
Daniel Kondov <danielkondov91@gmail.com>
2018-09-02 22:46:03 +0300

    Decorator

---
+++ Decorator/Buyable.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Decorator
+{
+    public class Buyable : Decorator
+    {
+        private readonly int price;
+
+        public Buyable(LibraryItem item, int price) : base(item)
+        {
+            this.price = price;
+        }
+
+        public override void Display()
+        {
+            base.Display();
+            Console.WriteLine("Price: $" + this.price);
+        }
+    }
+}
---
+++ Decorator/Decorator.cs
@@ -0,0 +1,17 @@
+﻿namespace Decorator
+{
+    public abstract class Decorator : LibraryItem
+    {
+        public LibraryItem LibraryItem { get; private set; }
+
+        protected Decorator(LibraryItem libraryItem)
+        {
+            this.LibraryItem = libraryItem;
+        }
+
+        public override void Display()
+        {
+            this.LibraryItem.Display();
+        }
+    }
+}
