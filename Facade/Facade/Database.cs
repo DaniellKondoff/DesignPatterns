b9b398a9f7e9fa5444a9d5845a48055541d8e97b
Daniel Kondov <danielkondov91@gmail.com>
2018-09-03 20:26:46 +0300

    Facade

---
+++ Facade/Facade/Database.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace Facade
+{
+    public class Database
+    {
+        private readonly ICollection<MediaEntity> availableMedia;
+
+        public Database()
+        {
+            this.availableMedia = new List<MediaEntity>();
+        }
+
+        public void Add(MediaEntity media)
+        {
+            this.availableMedia.Add(media);
+        }
+
+        public IEnumerable<MediaEntity> GetAvailableMedia()
+        {
+            return this.availableMedia;
+        }
+    }
+}
---
+++ Facade/Facade/StartUp.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Facade
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var homeTheaterPro = HomeTheaterPro.CreateInstance();
+            homeTheaterPro.InitHomeSystem();
+            homeTheaterPro.DisplayAvailableMedia();
+            homeTheaterPro.Next();
+            homeTheaterPro.Next();
+            homeTheaterPro.Previous();
+            homeTheaterPro.Stop();
+        }
+    }
+}
