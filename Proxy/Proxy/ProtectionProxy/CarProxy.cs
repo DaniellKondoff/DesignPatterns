be5724ce4497fabc83fabb7c3950eea0fac51578
Daniel Kondov <danielkondov91@gmail.com>
2018-09-03 20:55:34 +0300

    Proxy

---
+++ Proxy/Proxy/ProtectionProxy/CarProxy.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Proxy.ProtectionProxy
+{
+    public class CarProxy : ICar
+    {
+        private Car car = new Car();
+        private Driver driver;
+
+        public CarProxy(Driver driver)
+        {
+            this.driver = driver;
+        }
+
+        public void Drive()
+        {
+            if (driver.Age >= 16)
+                car.Drive();
+            else
+            {
+                Console.WriteLine("Driver too young");
+            }
+        }
+    }
+}
---
+++ Proxy/Proxy/StartUp.cs
@@ -0,0 +1,17 @@
+﻿using Proxy.ProtectionProxy;
+using System;
+
+namespace Proxy
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            //Protection Proxy
+            ICar car = new CarProxy(new Driver(12)); // 22
+            car.Drive();
+
+
+        }
+    }
+}
