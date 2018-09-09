eb36513ad6f79363785d19f9f3132d47e2c75262
Daniel Kondov <danielkondov91@gmail.com>
2018-09-09 16:32:42 +0300

    Command Pattern

---
+++ Command/Command/StartUp.cs
@@ -0,0 +1,31 @@
+﻿using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Command
+{
+    class StartUp
+    {
+        static void Main(string[] args)
+        {
+            var ba = new BankAccount();
+            var commands = new List<BankAccountCommand>
+            {
+                new BankAccountCommand(ba, Action.Deposit, 100),
+                new BankAccountCommand(ba, Action.Withdraw, 1000)
+            };
+
+            WriteLine(ba);
+
+            foreach (var c in commands)
+                c.Call();
+
+            WriteLine(ba);
+
+            foreach (var c in Enumerable.Reverse(commands))
+                c.Undo();
+
+            WriteLine(ba);
+        }
+    }
+}
