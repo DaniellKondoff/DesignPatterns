4b6f73e01c728ae439c98110f9005a4c0c48c5e5
Daniel Kondov <danielkondov91@gmail.com>
2018-09-25 20:10:55 +0300

    Visitor

---
+++ Visitor/Visitor/IVisitor.cs
@@ -0,0 +1,9 @@
+﻿namespace Visitor
+{
+    public interface IVisitor
+    {
+        void Visit(RealEstate realEstate);
+        void Visit(BankAccount bankAccount);
+        void Visit(Loan loan);
+    }
+}
